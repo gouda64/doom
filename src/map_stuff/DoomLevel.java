d142ce9dfe8cae7e26c009380d6cb418c4d3cc82
Erica Dong <erica3280cell@gmail.com>
2023-05-20 12:02:06 -0400

    Scale map to be less cramped

--- src/map_stuff/DoomLevel.java
+++ src/map_stuff/DoomLevel.java
@@ -33,7 +33,7 @@ public class DoomLevel {
 
     public DoomLevel(String mapFile, int width, int height, double renderDistToHeight, double scale) {//double xScale, double zScale) {
         this.scale = scale;
-        readMap(mapFile);
+        readMap(mapFile, 3, 3); //scale vs hor/ver scale serve diff purposes!!
 
         renderDist = mapHeight*renderDistToHeight;
 
@@ -249,14 +249,14 @@ public class DoomLevel {
         }
     }
 
-    public void readMap(String fileName) {
+    public void readMap(String fileName, double horScale, double vertScale) {
         try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
             String s;
             while ((s = in.readLine()) != null) {
                 String[] split = s.split(" ");
                 switch (split[0]) {
                     case "v" -> {
-                        vertices.add(new Point(Double.parseDouble(split[1]), 0, Double.parseDouble(split[2])));
+                        vertices.add(new Point(Double.parseDouble(split[1])*horScale, 0, Double.parseDouble(split[2])*vertScale));
                     }
                     case "e" -> {
                         Edge e = new Edge(vertices.get(Integer.parseInt(split[2])),
@@ -270,15 +270,15 @@ public class DoomLevel {
                         mapHeight = Double.parseDouble(split[1]);
                     }
                     case "m" -> {
-                        Point pos = new Point(Double.parseDouble(split[2]), 0, Double.parseDouble(split[3]));
+                        Point pos = new Point(Double.parseDouble(split[2])*horScale, 0, Double.parseDouble(split[3])*vertScale);
                         monsters.add(new Monster(Integer.parseInt(split[1]), pos));
                     }
                     case "i" -> {
-                        Point pos = new Point(Double.parseDouble(split[2]), 0, Double.parseDouble(split[3]));
+                        Point pos = new Point(Double.parseDouble(split[2])*horScale, 0, Double.parseDouble(split[3])*vertScale);
                         sprites.add(new Item(Integer.parseInt(split[1]), pos));
                     }
                     case "w" -> {
-                        Point pos = new Point(Double.parseDouble(split[2]), 0, Double.parseDouble(split[3]));
+                        Point pos = new Point(Double.parseDouble(split[2])*horScale, 0, Double.parseDouble(split[3])*vertScale);
                         sprites.add(new Weapon(Integer.parseInt(split[1]), pos));
                     }
                     case "we" -> {
@@ -286,7 +286,7 @@ public class DoomLevel {
                                 vertices.get(Integer.parseInt(split[2])));
                     }
                     case "p" -> {
-                        playerStart = new Point(Double.parseDouble(split[1]), Double.parseDouble(split[3]), Double.parseDouble(split[2]));
+                        playerStart = new Point(Double.parseDouble(split[1])*horScale, Double.parseDouble(split[3]), Double.parseDouble(split[2])*vertScale);
                     }
                     case "pv" -> {
                         playerLook = new Point(Double.parseDouble(split[1]), 0, Double.parseDouble(split[2]));
